57df96e9e8bd42bb079ec36e10f215ff6638cc48
Jovan Suberic <jovan.suberic@fioneer.com>
2024-04-19 16:35:29 +0200

    update nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { setTheme } from '@ui5/webcomponents-base/dist/config/Theme.js'
 import './App.css'
 import Header from './components/Header/Header.tsx'
+import '@ui5/webcomponents-icons/dist/AllIcons'
+
 
 function App() {
+    setTheme('sap_horizon')
   return (
     <>
         <Header />
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,30 +1,61 @@
 import {
     Avatar,
     Icon,
-    Input,
+    Input, List,
+    Modals,
     ShellBar,
     ShellBarItem,
+    StandardListItem,
 } from "@ui5/webcomponents-react";
 
-const Header = () => (
-    <ShellBar
-        logo={<Icon name="border" />}
-        onLogoClick={()=> {}}
-        onProfileClick={()=> {}}
-        onSearchButtonClick={function _a(){}}
-        primaryTitle="Spare Square"
-        profile={<Avatar><Icon name="border"/></Avatar>}
-        searchField={<Input icon={<Icon interactive name="search"/>} showClearIcon/>}
+const Header = () => {
+    const showPopover = Modals.useShowPopover();
+
+    return(
+        <ShellBar
+            logo={<Icon name="monitor-payments" />}
+            onLogoClick={()=> {}}
+            onProfileClick={() => {
+                showPopover({
+                    opener: 'shellbar-avatar',
+                    placementType: 'Bottom',
+                    horizontalAlign: 'Right',
+                    children: (<List separators="None" data-testid="userMenu">
+                        <StandardListItem
+                            icon="user-settings"
+                            onClick={() => {
+                            }}
+                            data-testid="userMenuSettings"
+                        >
+                            Profile Settings
+                        </StandardListItem>
+                        <StandardListItem
+                            icon="log"
+                            onClick={() => {
+                            }
+                            }
+                        >
+                            Logout
+                        </StandardListItem>
+                    </List>)
+                });
+            }}
+            onSearchButtonClick={()=>{}}
+            primaryTitle="Spare Square"
+            profile={<Avatar id="shellbar-avatar"><Icon name="person-placeholder"/></Avatar>}
+            searchField={<Input icon={<Icon interactive name="search"/>} showClearIcon/>
+}
     >
         <ShellBarItem
-            icon="border"
+            icon="qr-code"
             text="QR Scan"
         />
         <ShellBarItem
-            icon="bell"
+            icon="home"
             text="Home"
         />
     </ShellBar>
 )
+}
 
 export default Header
