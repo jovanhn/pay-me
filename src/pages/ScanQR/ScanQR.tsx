8de47ab3892f35890b42fdb2f34eb40adc3be27a
Jovan Suberic <jovan.suberic@fioneer.com>
2024-05-12 21:47:57 +0200

    manual

--- src/pages/ScanQR/ScanQR.tsx
+++ src/pages/ScanQR/ScanQR.tsx
@@ -1,13 +1,24 @@
 import {Scanner} from "@yudiel/react-qr-scanner";
-import {Loader, Text} from "@ui5/webcomponents-react";
+import {Button, Loader, Text} from "@ui5/webcomponents-react";
 import {useState} from "react";
 import {useInvoiceProcessor} from "../../services/invoice-processor.tsx";
 
+const url = "https://suf.purs.gov.rs/v/?vl=AzlWTUVLTVNOREJWOEdQTzA2FjwASxM8AEBCDwAAAAAAAAABjxpRbD8AAAAoRjk1cDtav%2FKlSwzrhQssXEjOfkqzH6hpmh9zYRVXvhkhD6ukvvO8oibiza7eK9EA6APXbqCvwevn1%2B9ZhozeVLlza3cCMfGG3qE5DBhU9%2FqWMa4kz2oK9HvGg33QVnfqrhO7TC26qKFi%2BCpmfjwwrq37x%2Fl%2FxsnRh71QrXkf2XlBQ5RX68brZTOvAKqn7L27FajTemGox5MUqxZPOlsgTKKBPhmccfTQSb0lxFqB%2FuyjVwJJtb0KHx%2FyMk53%2F53wXE90VvvmsBTtSQFL30G5kEgXhCICx4yEGnqaRux81kXlExmeZzbgIp7UjrruiBpXWAvkUZnbGxpcvggXBFRkuA9%2FfQS01QMKmvfO8RSM%2F6YMqMarjYjbMhanbg%2FxYJ%2Bsc2VHLYj21XXgYkFWTX7789VNgEO24adz5VN2%2FkBGvXx%2BfhDpsY1btscYZSV%2BH%2FDkNxXZmmdAYjkF2aCQSN9oXBTZZJlZG2RpXrUAMwM8yUAIChDruDQgKT3pRvGdE%2FwVU1F%2BNI1L4GPCy5MSX7tCjq1TdfAOE2BpsMsQBfGRRnif%2Bapx7WfpJaLGx7TpA3EnGgM1qqMXwNSys%2F%2FzOpIXpqgA%2FPdA2p0CkKLmZPySJbpkVN%2FqQsuDxid7zuiTWYN%2FZXITThN%2BlSRCBunOBJ%2BgFMZ4sg4UD83RZLrZ6a1feXz6tWt9X4oox2MB%2FhQ3CXM%3D"
 const ScanQR = () => {
     const [qrCode, setQrCode] = useState("");
     const {data, error, isLoading, processInvoice} = useInvoiceProcessor();
     const [executed, setExecuted] = useState(false);
     return (<>
+            <Button onClick={()=> {
+                if(!executed) {
+                    setExecuted(true);
+                    setQrCode(url);
+                    processInvoice(url).then(() => {
+                        setQrCode('Done')
+                    })
+                    console.log(url);
+                }
+            }}>Manual create</Button>
             <Scanner
                 onResult={(text, result) => {
                     if (text.startsWith("https://suf.purs.gov.rs/")) {
