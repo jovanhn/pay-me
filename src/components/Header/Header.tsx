4922470b3e5dc8452e370986347aed11d0dd3fe1
Jovan Suberic <jovan.suberic@fioneer.com>
2024-05-18 10:35:53 +0200

    fix

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,31 +8,37 @@ import {
     StandardListItem,
 } from "@ui5/webcomponents-react";
 import {useNavigate} from "react-router-dom";
+import {useState} from "react";
 
 const Header = () => {
     const showPopover = Modals.useShowPopover();
     const navigate = useNavigate()
+    const [isOpen, setOpen] = useState<boolean>()
 
-    return(
+    return (
         <ShellBar
-            logo={<Icon name="monitor-payments" />}
-            onLogoClick={ () => navigate('/')}
+            logo={<Icon name="monitor-payments"/>}
+            onLogoClick={() => navigate('/')}
             onProfileClick={() => {
                 showPopover({
+                    open: isOpen,
                     opener: 'shellbar-avatar',
                     placementType: 'Bottom',
                     horizontalAlign: 'Right',
                     children: (<List separators="None" data-testid="userMenu">
                         <StandardListItem
                             icon="user-settings"
-                            onClick={() => navigate('/profile')}
+                            onClick={() => {
+                                setOpen(false)
+                                navigate('/profile')
+                            }}
                             data-testid="userMenuSettings"
                         >
                             Profile Settings
                         </StandardListItem>
                         <StandardListItem
                             icon="log"
-                            onClick={()=> {
+                            onClick={() => {
 
 
                             }}
@@ -42,24 +48,25 @@ const Header = () => {
                     </List>)
                 });
             }}
-            onSearchButtonClick={()=>{}}
+            onSearchButtonClick={() => {
+            }}
             primaryTitle="Spare Square"
             profile={<Avatar id="shellbar-avatar"><Icon name="person-placeholder"/></Avatar>}
             searchField={<Input icon={<Icon interactive name="search"/>} showClearIcon/>
-}
-    >
-        <ShellBarItem
-            icon="qr-code"
-            text="QR Scan"
-            onClick={() => navigate('/scan-qr')}
-        />
-        <ShellBarItem
-            icon="home"
-            text="Home"
-            onClick={ () => navigate('/')}
-        />
-    </ShellBar>
-)
+            }
+        >
+            <ShellBarItem
+                icon="qr-code"
+                text="QR Scan"
+                onClick={() => navigate('/scan-qr')}
+            />
+            <ShellBarItem
+                icon="home"
+                text="Home"
+                onClick={() => navigate('/')}
+            />
+        </ShellBar>
+    )
 }
 
 export default Header
--- src/services/invoice-processor.tsx
+++ src/services/invoice-processor.tsx
@@ -20,12 +20,14 @@ export const useInvoiceProcessor = () => {
             // this spins server if it is down
             // await axios.get('https://invoice-processor.onrender.com/')
 
-            const response = await axios.post('https://invoice-processor.onrender.com/process', {url: qr_text});
-            const dateString = response.data.dateTime;
-
+            const response = await axios.post('https://invoice-processor.onrender.com/dummy/process', {url: qr_text});
+            const dateString = response.data.dateTime.split(' ')[0];
+            const timeString = response.data.dateTime.split(' ')[1];
             // Split the date string into its components
-            const [day, month, year, time] = dateString.split(/[.\s]+/);
-            const dateObject = new Date(`${year}-${month}-${day}T${time}`);
+
+            const [day, month, year] = dateString.split('.')
+
+            const dateObject = new Date(`${year}-${month}-${day}T${timeString}`);
 
             // Convert the JavaScript Date object to a Firestore Timestamp
             const timestamp = Timestamp.fromDate(dateObject);
@@ -36,7 +38,7 @@ export const useInvoiceProcessor = () => {
             const invoice = response.data as Invoice;
             invoice.id = crypto.randomUUID()
             console.log(invoice.totalAmount)
-            invoice.totalAmount = Number(invoice.totalAmount.toString().replace(',','.'))
+            invoice.totalAmount = Number(invoice.totalAmount.toString().replace('.','').replace(',','.'))
             await setDoc(doc(db, `data/invoices/${user.uid}`, invoice.id), invoice).then((response) => {
                 console.log("Invoice created");
                 console.log(response)
